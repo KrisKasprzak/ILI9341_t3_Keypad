0145c4d8033224b73ea26c04b74f89b9759234a1
Kris Kasprzak <kris.kasprzak@yahoo.com>
2024-05-01 12:18:13 -0500

    Update Keyboard.ino

--- Examples/Keyboard/Keyboard.ino
+++ Examples/Keyboard/Keyboard.ino
@@ -1,8 +1,6 @@
 // required libraries
 #include "ILI9341_t3.h"  // high speed display that ships with Teensy
 #include "font_Arial.h"  // custom fonts that ships with ILI9341_t3.h
-#include <font_ArialBold.h>
-#include <ILI9341_t3_Controls.h>
 #include <ILI9341_t3_Keypad.h>
 #include <XPT2046_Touchscreen.h>
 
@@ -12,201 +10,67 @@
 #define RST_PIN 8
 #define T_CS 0
 #define T_IRQ 1
-#define SD_CS A0
+
 // easy way to include fonts but change globally
 #define FONT_BUTTON Arial_16  // font for keypad buttons
 
-int BtnX = 0, BtnY = 0;
-int i = 0;
+// you will probably need to calibrate your screen, these are coordinates of presses on display
+uint16_t ScreenLeft = 350, ScreenRight = 3900, ScreenTop = 300, ScreenBottom = 3800;
 
-// you know the drill
-ILI9341_t3 Display(CS_PIN, DC_PIN, RST_PIN);  //Display object
+
+ILI9341_t3 Display(CS_PIN, DC_PIN, RST_PIN);
 
 XPT2046_Touchscreen Touch(T_CS, T_IRQ);
-TS_Point p;
 
-// create some UI buttons
-Button ShowPassword(&Display);
-Button ShowNumberPad(&Display);
-Button ShowKeyboard(&Display);
+TS_Point TP;
 
-// create some keypad objects
-NumberPad PasswordInput(&Display, &Touch);
-NumberPad MyNumberPad(&Display, &Touch);
-Keyboard MyKeyboard(&Display, &Touch);
 
+Keyboard SSIDPasswordPad(&Display, &Touch);
 
 
 void setup() {
 
-  Serial.begin(9600);
 
-  // while ((millis() > 5000) || (!Serial)) {}
+  Serial.begin(115200);
 
-  // fire up the display
   Display.begin();
-  Display.setRotation(1);
+
+  Display.setRotation(3);
+  Display.fillScreen(ILI9341_BLACK);
 
   Touch.begin();
-  Touch.setRotation(1);
+  Touch.setRotation(3);
 
-  Display.fillScreen(ILI9341_BLACK);
+  SSIDPasswordPad.init(ILI9341_BLACK, ILI9341_BLACK, ILI9341_LIGHTGREY, ILI9341_WHITE, ILI9341_DARKGREY, FONT_BUTTON);
+  SSIDPasswordPad.setTouchLimits(ScreenLeft, ScreenRight, ScreenTop, ScreenBottom);
 
-  ShowPassword.init(160, 50, 150, 50, ILI9341_NAVY, ILI9341_BLUE, ILI9341_WHITE, ILI9341_BLACK, "Password", 0, 0, FONT_BUTTON);
-  ShowNumberPad.init(160, 120, 150, 50, ILI9341_NAVY, ILI9341_BLUE, ILI9341_WHITE, ILI9341_BLACK, "Number pad", 0, 0, FONT_BUTTON);
-  ShowKeyboard.init(160, 190, 150, 50, ILI9341_NAVY, ILI9341_BLUE, ILI9341_WHITE, ILI9341_BLACK, "Keyboard", 0, 0, FONT_BUTTON);
+  // optional
+  // SSIDPasswordPad.setCornerRadius(5);  
+  SSIDPasswordPad.setDisplayColor(40, 48520);
+  // SSIDPasswordPad.hideInput();
+  // SSIDPasswordPad.clearInput();
+  SSIDPasswordPad.setInitialText("Enter IP Address");
 
-  // initialize the numberpad object
-  // let's simulate a password control (pure numbers only)
-  PasswordInput.init(ILI9341_BLACK, ILI9341_WHITE, ILI9341_BLUE, ILI9341_NAVY, ILI9341_WHITE, ILI9341_CYAN, ILI9341_YELLOW, FONT_BUTTON);
-  PasswordInput.hideInput();
-  PasswordInput.enableDecimal(false);
-  PasswordInput.enableNegative(false);
-  PasswordInput.setInitialText("Enter password");
 
-  // initialize the numberpad object
-  // simulate a good old fashioned number input
-  MyNumberPad.init(ILI9341_BLACK, ILI9341_WHITE, ILI9341_BLUE, ILI9341_NAVY, ILI9341_WHITE, ILI9341_CYAN, ILI9341_YELLOW, FONT_BUTTON);
-  // optional methods
-  // MyNumberPad.enableDecimal(bool State); // disable / enable decimal point (enabled by default)
-  // MyNumberPad.enableNegative(bool State); // disable / enable negative sign (enabled by default)
-  // MyNumberPad.setMinMax(float MininumValue, float MaximumValue); // want bound checks?
-  // use the value property to set the initial value if desired
-  MyNumberPad.value = 3.14159;
-
-  // initialize the keyboard object
-  // simulate a good old fashioned keyboard
-  MyKeyboard.init(ILI9341_BLACK, ILI9341_WHITE, ILI9341_BLUE, ILI9341_NAVY, ILI9341_WHITE, ILI9341_CYAN, ILI9341_YELLOW, FONT_BUTTON);
-  // optional methods
-  // MyKeyboard.setDisplayColor(uint16_t TextColor, uint16_t BackColor); change colors
-  // MyKeyboard.hideInput(); // for expanded password input
-  // MyKeyboard.setInitialText(const char *Text); // maybe show and ip address format
-  // MyKeyboard.setInitialText("IP 111.222.333.444");
-  // optional to populate the input box
-  // strcpy(MyKeyboard.data, "TEXT");
-
-  drawMenu();
-
-}
-
-void drawMenu() {
-  Display.fillScreen(ILI9341_BLACK);
-  ShowPassword.draw();
-  ShowNumberPad.draw();
-  ShowKeyboard.draw();
-  delay(500);
 }
 
 void loop() {
 
-  if (Touch.touched()) {
+  SSIDPasswordPad.getInput();
 
-    ProcessTouch();
+  Display.fillScreen(ILI9341_BLACK);
 
-    if (ProcessButtonPress(ShowPassword)) {
-      Display.fillScreen(ILI9341_BLACK);
-      PasswordInput.getInput();
+  Serial.print("data method 1 ");
+  Serial.println(SSIDPasswordPad.data);
 
-      Display.fillScreen(ILI9341_BLACK);
-      Display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-      Display.setCursor(40, 50);
-      Display.print("PasswordInput.value is: ");
-      Display.setCursor(40, 100);
-      Display.print((uint32_t)PasswordInput.value);
-      Display.setCursor(40, 200);
-      Display.print("Pass screen to continue.");
-      PasswordInput.value = 0;
-      while (!Touch.touched()) {
-      }
-      drawMenu();
-    }
-    if (ProcessButtonPress(ShowNumberPad)) {
-      Display.fillScreen(ILI9341_BLACK);
-      MyNumberPad.getInput();
+  // or
+  const char *pw;
 
-      Display.fillScreen(ILI9341_BLACK);
-      Display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-      Display.setCursor(40, 50);
-      Display.print("MyNumberPad.value is: ");
-      Display.setCursor(40, 100);
-      Display.print(MyNumberPad.value);
-      Display.setCursor(40, 200);
-      Display.print("Pass screen to continue.");
-      MyNumberPad.value = 0;
-      while (!Touch.touched()) {
-      }
-      drawMenu();
-    }
-    if (ProcessButtonPress(ShowKeyboard)) {
-      Display.fillScreen(ILI9341_BLACK);
-      MyKeyboard.getInput();
+  pw = SSIDPasswordPad.data;
+  Serial.print("data method 2 ");
+  Serial.println(pw);
 
-      Display.fillScreen(ILI9341_BLACK);
-      Display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-      Display.setCursor(40, 50);
-      Display.print("MyKeyboard.data is: ");
-      Display.setCursor(40, 100);
-      Display.print(MyKeyboard.data);
-      Display.setCursor(40, 200);
-      Display.print("Pass screen to continue.");
+  SSIDPasswordPad.clearInput();
+  SSIDPasswordPad.setInitialText("Enter IP Address");
 
-      MyKeyboard.data[0] = '\0';
-      //strcpy(MyKeyboard.data, ' ');
-      while (!Touch.touched()) {
-      }
-      drawMenu();
-    }
-  }
 }
-
-void ProcessTouch() {
-
-  if (Touch.touched()) {
-    p = Touch.getPoint();
-    BtnX = p.x;
-    BtnY = p.y;
-
-#ifdef debug
-    Serial.print(" real coordinates:");
-    Serial.print(BtnX);
-    Serial.print(" ,");
-    Serial.print(BtnY);
-#endif
-
-    // different displays may require reversing last 2 args
-    BtnX = map(p.x, 3975, 169, 0, 320);
-    BtnY = map(p.y, 3850, 304, 0, 240);
-
-#ifdef debug
-    Serial.print(" , Mapped coordinates:");
-    Serial.print(BtnX);
-    Serial.print(" ,");
-    Serial.println(BtnY);
-    Display.fillCircle(BtnX, BtnY, 2, ILI9341_RED);
-#endif
-  }
-}
-
-bool ProcessButtonPress(Button TheButton) {
-
-  if (TheButton.press(BtnX, BtnY)) {
-
-    TheButton.draw(B_PRESSED);
-    while (Touch.touched()) {
-      if (TheButton.press(BtnX, BtnY)) {
-
-        TheButton.draw(B_PRESSED);
-      } else {
-        TheButton.draw(B_RELEASED);
-        return false;
-      }
-      ProcessTouch();
-    }
-
-    TheButton.draw(B_RELEASED);
-    return true;
-  }
-  return false;
-}
-
-
